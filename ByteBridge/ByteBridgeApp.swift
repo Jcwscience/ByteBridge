ac549d63e2e8e3b875c3e90cd17bbb45a8ef1638
Jcwscience <jcwscience@me.com>
2023-09-26 08:50:48 -0400

    Initial Commit

---
+++ ByteBridge/ByteBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByteBridgeApp.swift
+//  ByteBridge
+//
+//  Created by John on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ByteBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
